2c18d673884fd4db00dcf2da55a990d8b923f841
Quan Nguyen <quanhong468@gmail.com>
2024-07-21 18:22:23 +0700

    Query Builder

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -6,6 +6,8 @@ const {
     getAllCustomerService, putUpdateCustomerService, deleteCustomerService, deleteArrayCustomerService
 } = require("../services/customerService");
 
+const aqp = require('api-query-params');
+
 module.exports = {
     postCreateCustomer: async (req, res) => {
         let {name, address, phone, email, description} = req.body;
@@ -55,14 +57,14 @@ module.exports = {
         }
     },
     getAllCustomers: async (req, res) => {
-        console.log(req.query)
+
         let limit = req.query.limit;
         let page= req.query.page;
         let name = req.query.name;
         let result = null;
 
         if(limit && page){
-            result = await getAllCustomerService(limit,page,name)
+            result = await getAllCustomerService(limit, page, name, req.query)
         } else
             result = await getAllCustomerService()
         return res.status(200).json(
